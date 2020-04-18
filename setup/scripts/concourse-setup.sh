a798e9d4e272a7462da14a2c6dec6fedf86764a6
Aravindjeev <40687067+Aravindjeev@users.noreply.github.com>
2020-04-18 21:32:25 +0530

    Delete concourse-setup.sh

--- setup/scripts/concourse-setup.sh
+++
@@ -1,4 +0,0 @@
-fly -t main login --team-name main --concourse-url http://concourse.rancher.boxboat.io -u test -p test
-fly -t main set-pipeline --pipeline vault-setup -c ci/pipeline.yml
-fly -t main unpause-pipeline -p vault-setup
-fly -t main ep -p vault-setup
